8e45d6f9046006544be908f78e11b45f549542af
shrikantpatel <shrikant_patelin@yahoo.com>
2023-10-01 12:52:59 -0500

    Basic structure for pair sum created.

---
+++ Pair_Matches_Sum.py
@@ -0,0 +1,15 @@
+def main() :
+
+        numList = [1, 2, 3, 4, 4]
+        sum = 8
+        pairExist = False
+
+        # set the pairExist to true or false based on if the sum exist in the numList
+
+
+        #
+
+        print('answer is - ',pairExist)
+        
+if __name__ == "__main__" :
+    main()
