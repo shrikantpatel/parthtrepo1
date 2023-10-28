ba6905afae79fac8742b9526eb4e6364fd700293
shrikantpatel <shrikant_patelin@yahoo.com>
2023-10-28 12:22:15 -0500

    3rd Part of Tic Tac Toe Game

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,10 +13,16 @@ markers = []
 clicked = False
 pos = []
 player = 1
+game_over = False
+
 
 #define coulours
 green = (0, 255, 0)
 red = (255, 0, 0)
+
+#define font
+font = pygame.font.SysFont(None, 40)
+blue = (0, 0, 255)
 
 #draw the grid
 def draw_grid():
@@ -42,7 +48,7 @@ def draw_markers():
       for y in x:
          #if in that cell the marker belongs to player one...
          if y== 1:
-            #then draw player ones icon
+            #then draw player ones icon   
             pygame.draw.line(screen, green, (x_pos * 100 + 15 , y_pos * 100 + 15), (x_pos * 100 + 85, y_pos * 100 + 85), line_width)
             pygame.draw.line(screen, green, (x_pos * 100 + 15 , y_pos * 100 + 85), (x_pos * 100 + 85, y_pos * 100 + 15), line_width)
          #if y = -1 than is player two:
@@ -53,7 +59,44 @@ def draw_markers():
       #one were moved on from the y I want to move on to the x postion
       x_pos += 1
       
-            
+def check_winner():
+   
+   global winner
+   global game_over
+   
+   y_pos = 0
+   for x in markers:
+      #check columns
+      if sum(x) == 3:
+         winner = 1
+         game_over = True
+      if sum(x) == -3:
+         winner = 2
+         game_over = True
+      # check all the rows
+      if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos]==3:
+         winner = 1
+         game_over = True
+      if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos]==-3:
+         winner = 2
+         game_over = True
+      y_pos += 1
+      
+   #check cross
+   if markers[0][0] + markers[1][1] + markers[2][2] == 3 or markers[2][0] + markers[1][1] + markers[0][2] == 3:
+      winner = 1
+      game_over = True
+   if markers[0][0] + markers[1][1] + markers[2][2] == -3 or markers[2][0] + markers[1][1] + markers[0][2] == -3:
+      winner = 1
+      game_over = True
+      
+      
+def draw_winner(winner):
+   win_text = 'Player ' + str(winner) +' wins!'
+   win_img = font.render(win_text, True, blue)
+   pygame.draw.rect(screen, green, (screen_width // 2 - 100, screen_height // 2 - 60, 200, 50))
+   screen.blit(win_img, (screen_width // 2 - 100, screen_height // 2 - 50))
+   
 
 #this while loop will run the game and look for interaction from the user
 run = True
@@ -68,25 +111,29 @@ while run:
       if event.type == pygame.QUIT:
          run = False
       #this will run a click cycle and take it as an input
-      if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
-         #when we click the mouse the variable clicked will become true
-         clicked = True
-      if event.type == pygame.MOUSEBUTTONUP and clicked == True:  
-         #when we let go of the mouse button then the variable will become false again
-         clicked = False
-         #now we have to take the position of where the mouse was when if was clicked
-         pos = pygame.mouse.get_pos()
-         #now we need to extract the positions of x and y
-         cell_x = pos[0]
-         cell_y = pos[1]
-         #now we need to see in which square the mouse is in 
-         #our window size is 300 by 300 so if we divide by 100 than we can get the postion of the square
-         #// allows us to do floor division which will give us a round number
-         # EX. If we divide 5 by 2 using integer division, the result would be 2.
-         if markers[cell_x // 100][cell_y //100] == 0:
-            markers[cell_x // 100][cell_y //100] = player
-            player *= -1
-         
+      if game_over == 0:
+         if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+            #when we click the mouse the variable clicked will become true
+            clicked = True
+         if event.type == pygame.MOUSEBUTTONUP and clicked == True:  
+            #when we let go of the mouse button then the variable will become false again
+            clicked = False
+            #now we have to take the position of where the mouse was when if was clicked
+            pos = pygame.mouse.get_pos()
+            #now we need to extract the positions of x and y
+            cell_x = pos[0]
+            cell_y = pos[1]
+            #now we need to see in which square the mouse is in 
+            #our window size is 300 by 300 so if we divide by 100 than we can get the postion of the square
+            #// allows us to do floor division which will give us a round number
+            # EX. If we divide 5 by 2 using integer division, the result would be 2.
+            if markers[cell_x // 100][cell_y //100] == 0:
+               markers[cell_x // 100][cell_y //100] = player
+               player *= -1
+               check_winner()
+   
+   if game_over == True:
+      draw_winner(winner)      
          
    #will update the code if any addition are made (images, etc)   
    pygame.display.update()
