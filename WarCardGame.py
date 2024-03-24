05f85da0647fd538bce905192a16781e8962494d
shrikantpatel <shrikant_patelin@yahoo.com>
2024-03-24 14:07:27 -0500

    Made class deck, player, and card for war

---
+++ WarCardGame.py
@@ -0,0 +1,55 @@
+#CARD
+#SUIT, RANK, VALUE\
+import random
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11,
+         'Queen':12, 'King':13, 'Ace':14}
+
+class Card():
+    #User will need to tell us the suit and rank of the card
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    #Make card more human readable using string
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+
+class Deck():
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit,rank)
+                # this is where we will create the card object
+                self.all_cards.append(created_card)
+    
+    def shuffle(self):
+        
+        random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        return self.all_cards.pop()
+    
+    
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+        
+    def remove_one(self):
+        return self.all_cards.pop(0)
+    
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            #FOR MULTIPLE CARD OBJECYS
+            self.all_cards.extend(new_cards)
+        else:
+            #fOR A SINGLE CARD OBJECT
+            self.all_cards.append(new_cards)
+            
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+    
